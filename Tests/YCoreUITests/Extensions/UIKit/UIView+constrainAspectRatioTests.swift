07ade0d91c3fc60792ade46821978d934da6a858
Dev karan <dev.karan@ymedialabs.com>
2022-12-14 18:48:56 +0530

    CM:1012: Added "UIView+constrainAspectRatioTests.swift"

---
+++ Tests/YCoreUITests/Extensions/UIKit/UIView+constrainAspectRatioTests.swift
@@ -0,0 +1,14 @@
+//
+//  UIView+constrainAspectRatioTests.swift
+//  YCoreUI
+//
+//  Created by Dev Karan on 14/12/22.
+//  Copyright © 2022 Y Media Labs. All rights reserved.
+//
+
+import XCTest
+@testable import YCoreUI
+
+final class UIViewContrainAspectRatioTests: XCTestCase {
+    
+}
