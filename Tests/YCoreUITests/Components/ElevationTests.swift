20192a3ab7dd911c715559f16ce02eed3ca08b5f
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2022-12-14 18:54:13 +0530

    test elevation init and apply methods

--- Tests/YCoreUITests/Components/ElevationTests.swift
+++ Tests/YCoreUITests/Components/ElevationTests.swift
@@ -9,8 +9,95 @@
 import XCTest
 @testable import YCoreUI
 
+struct Elevation {
+    let offset: CGSize
+    let blur: CGFloat
+    let spread: CGFloat
+    let color: UIColor
+    let opacity: CGFloat
+    let useShadowPath: Bool
+    
+    init(
+        offset: CGSize,
+        blur: CGFloat,
+        spread: CGFloat,
+        color: UIColor,
+        opacity: CGFloat,
+        useShadowPath: Bool = true
+    ) {
+        self.offset = offset
+        self.blur = blur
+        self.spread = spread
+        self.color = color
+        self.opacity = opacity
+        self.useShadowPath = useShadowPath
+    }
+    
+    func apply(layer: CALayer) {
+        guard useShadowPath else { return }
+        
+        layer.shadowPath = UIBezierPath().cgPath
+    }
+}
+
 final class ElevationTests: XCTestCase {
-    func test_failure() {
-        XCTFail("Failed")
+    func test_init_deliversGivenValues() {
+        let sut = makeSUT()
+        
+        XCTAssertEqual(sut.offset, CGSize(width: 1, height: 1))
+        XCTAssertEqual(sut.blur, 2)
+        XCTAssertEqual(sut.spread, 3)
+        XCTAssertEqual(sut.color, .red)
+        XCTAssertEqual(sut.opacity, 5)
+        XCTAssertEqual(sut.useShadowPath, true)
+    }
+    
+    func test_init_defaultValue() {
+        let sut = Elevation(
+            offset: CGSize(width: 1, height: 1),
+            blur: 2,
+            spread: 3,
+            color: .red,
+            opacity: 5
+        )
+        
+        XCTAssertEqual(sut.useShadowPath, true)
+    }
+    
+    func test_apply_doesNotAddShadowPathWhenUseShadowPathIsFalse() {
+        let sut = makeSUT(useShadowPath: false)
+        
+        let layer = CALayer()
+        
+        sut.apply(layer: layer)
+        
+        XCTAssertNil(layer.shadowPath)
+    }
+    
+    func test_apply_addsShadowPathWhenUseShadowPathIsTrue() {
+        let sut = makeSUT(useShadowPath: true)
+        
+        let layer = CALayer()
+        
+        sut.apply(layer: layer)
+        
+        XCTAssertNotNil(layer.shadowPath)
+    }
+    
+    func makeSUT(offset: CGSize = CGSize(width: 1, height: 1),
+                 blur: CGFloat = 2,
+                 spread: CGFloat = 3,
+                 color: UIColor = .red,
+                 opacity: CGFloat = 5,
+                 useShadowPath: Bool = true
+    ) -> Elevation {
+        Elevation(
+            offset: offset,
+            blur: blur,
+            spread: spread,
+            color: color,
+            opacity: opacity,
+            useShadowPath: useShadowPath
+        )
     }
 }
