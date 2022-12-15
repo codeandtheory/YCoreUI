0f2153617f0fcf8a21f8d0147fe9c85f2f847dfe
Karthik K Manoj <karthik.manoj@ymedialabs.com>
2022-12-15 11:04:46 +0530

    add documentation comments to `Elevation`

--- Sources/YCoreUI/Components/Elevation.swift
+++ Sources/YCoreUI/Components/Elevation.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
+/// Encapsulate design shadows and apply them to view.
 public struct Elevation {
+    /// The offset of the layer’s shadow.
     public let offset: CGSize
+    /// The blur of the layers. shadow.
     public let blur: CGFloat
+    /// The spread of the layers. shadow.
     public let spread: CGFloat
+    /// The color of the layer’s shadow.
     public let color: UIColor
+    /// The opacity of the layer’s shadow.
     public let opacity: CGFloat
+    /// Flag to set shadow path. Default is `true`.
     public let useShadowPath: Bool
     
+    /// Initializes `Elevation`.
+    /// - Parameters:
+    ///   - offset: the offset of the layer’s shadow
+    ///   - blur: the blur of the layers. shadow
+    ///   - spread: the spread of the layers. shadow
+    ///   - color: the color of the layer’s shadow
+    ///   - opacity: the opacity of the layer’s shadow
+    ///   - useShadowPath: flag to set shadow path. Default is `true`
     public init(
         offset: CGSize,
         blur: CGFloat,
@@ -32,6 +47,10 @@ public struct Elevation {
         self.useShadowPath = useShadowPath
     }
     
+    /// Applies elevation to a layer.
+    /// - Parameters:
+    ///   - layer: layer of the corresponding view
+    ///   - cornerRadius: the radius to use when drawing rounded corners for the layer’s background.
     public func apply(layer: CALayer, cornerRadius: CGFloat) {
         guard useShadowPath else { return }
         
