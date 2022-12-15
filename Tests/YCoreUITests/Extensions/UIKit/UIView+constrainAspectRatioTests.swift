e3dc3dc3a4f1499dd3b3c0060dd97cfc258b4e85
Dev karan <dev.karan@ymedialabs.com>
2022-12-15 13:28:15 +0530

    CM-1012: UT in progress

--- Tests/YCoreUITests/Extensions/UIKit/UIView+constrainAspectRatioTests.swift
+++ Tests/YCoreUITests/Extensions/UIKit/UIView+constrainAspectRatioTests.swift
@@ -10,5 +10,37 @@ import XCTest
 @testable import YCoreUI
 
 final class UIViewContrainAspectRatioTests: XCTestCase {
+    func testSimple() {
+        let sut = makeSUT()
+        XCTAssert(sut.translatesAutoresizingMaskIntoConstraints)
+        sut.frame = CGRect(x: 0, y: 0, width: 100, height: 500)
+        sut.constrainAspectRatio(0.5)
+//        XCTAssertEqual(sut.frame.height, sut.frame.width * 0.5)
+    }
     
+    func testOffset() {
+        let sut = makeSUT()
+        var randomOffset: CGFloat = CGFloat.random(in: 0..<500)
+        let constraint = sut.constrainAspectRatio(0.5, offset: randomOffset)
+        XCTAssertEqual(constraint.constant, randomOffset)
+    }
+        
+    func testPriority() {
+        let sut = makeSUT()
+        let randomPriority: UILayoutPriority = UILayoutPriority(Float.random(in: 1...1000))
+        let constraint = sut.constrainAspectRatio(0.5, priority: randomPriority)
+        XCTAssertEqual(constraint.priority, randomPriority)
+    }
 }
+
+private extension UIViewContrainAspectRatioTests {
+    func makeSUT(
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> UIView {
+        let sut = UIView()
+        trackForMemoryLeak(sut, file: file, line: line)
+        return sut
+    }
+}
+
