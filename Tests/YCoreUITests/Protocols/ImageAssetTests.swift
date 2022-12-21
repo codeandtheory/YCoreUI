f1e62f66700f6597e9c0a9beaeb31d3c0e337e7c
Dev karan <dev.karan@ymedialabs.com>
2022-12-21 13:56:03 +0530

    CM-965: 'ImageAssetTests' created

---
+++ Tests/YCoreUITests/Protocols/ImageAssetTests.swift
@@ -0,0 +1,12 @@
+//
+//  ImageAssetTests.swift
+//  YCoreUI
+//
+//  Created by Dev Karan on 21/12/22.
+//  Copyright © 2022 Y Media Labs. All rights reserved.
+//
+
+import XCTest
+
+final class ImageAssetTests: XCTestCase {
+}
