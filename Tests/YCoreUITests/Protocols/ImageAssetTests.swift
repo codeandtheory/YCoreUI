050f54f61c75a635278f7677bd08379e3426f429
Dev karan <dev.karan@ymedialabs.com>
2022-12-22 12:12:58 +0530

    CM-965: Test for 'addImageAsset'

--- Tests/YCoreUITests/Protocols/ImageAssetTests.swift
+++ Tests/YCoreUITests/Protocols/ImageAssetTests.swift
@@ -7,6 +7,65 @@
 //
 
 import XCTest
+import YCoreUI
 
 final class ImageAssetTests: XCTestCase {
+    
+    func testBundle() {
+        XCTAssertEqual(Flags.bundle, .module)
+        XCTAssertEqual(Icons.bundle, .module)
+    }
+    
+    func testNamespace() {
+        XCTAssertNil(Flags.namespace)
+        XCTAssertEqual(Icons.namespace, "Icons")
+        XCTAssertNil(Missing.namespace)
+    }
+    
+    func testFallbackImage() {
+        XCTAssertNotNil(Flags.fallbackImage)
+        XCTAssertNotNil(Icons.fallbackImage)
+        XCTAssertEqual(Missing.fallbackImage, UIImage(systemName: "x.squareroot"))
+    }
+
+    func testLoadImageWithNameSpace() {
+        Icons.allCases.forEach {
+            XCTAssertNotNil($0.loadImage())
+        }
+    }
+    
+    func testLoadImageWithoutNameSpace() {
+        Flags.allCases.forEach {
+            XCTAssertNotNil($0.loadImage())
+        }
+    }
+}
+
+extension ImageAssetTests {
+    enum Flags: String, CaseIterable, ImageAsset {
+        case unitedStates = "flag_us"
+        case india = "flag_in"
+        case brazil = "flag_br"
+        case switzerland = "flag_ch"
+        
+        static var bundle: Bundle { .module }
+    }
+    
+    enum Icons: String, CaseIterable, ImageAsset {
+        case plus
+        case minus
+        
+        static var bundle: Bundle { .module }
+        static var namespace: String? { "Icons" }
+    }
+    
+    enum Missing: String, CaseIterable, ImageAsset {
+        case notHere
+        case gone
+        
+        static var fallbackImage: UIImage {
+            let image: UIImage! = UIImage(systemName: "x.squareroot")
+            return image
+        }
+    }
 }
